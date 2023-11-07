736949a29bf850e526ae165df3d883969963e8df
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-07 16:09:39 +0530

    modified

--- src/Assignments/Assignment-3/Ques2/LandingPage.jsx
+++ src/Assignments/Assignment-3/Ques2/LandingPage.jsx
@@ -3,12 +3,13 @@ import AuthConsumer from "./AuthConsumer";
 import PreferenceConsumer from "./PreferenceConsumer";
 import { PreferenceContext } from "./PreferenceContext";
 import './LandingPage.css'
+import { DARK, LIGHT } from "./CONSTANTS";
 
 const LandingPage = () => {
   const { theme } = useContext(PreferenceContext);
   return (
     <>
-      <div className={`${ theme==='LIGHT' ? 'LIGHT' : 'DARK'}`}>
+      <div className={`${ theme===LIGHT ? LIGHT : DARK}`}>
         <AuthConsumer />
         <PreferenceConsumer />
       </div>
--- src/Assignments/Assignment-3/Ques2/PreferenceContext.jsx
+++ src/Assignments/Assignment-3/Ques2/PreferenceContext.jsx
@@ -1,11 +1,12 @@
 import { createContext, useState } from "react"
+import { DARK, LIGHT } from "./CONSTANTS";
 
 export const PreferenceContext = createContext();
 export const PreferenceProvider = ({children})=>{
-    const [theme, setTheme] = useState("LIGHT");
+    const [theme, setTheme] = useState(LIGHT);
     const switchTheme = () => {
-        if (theme === "LIGHT") setTheme("DARK");
-        else setTheme("LIGHT");
+        if (theme === LIGHT) setTheme(DARK);
+        else setTheme(LIGHT);
       };
     return(
         <PreferenceContext.Provider value={{ theme, switchTheme }}>
