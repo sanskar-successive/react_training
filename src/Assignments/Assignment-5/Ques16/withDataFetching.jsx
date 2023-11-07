acf8f275053094e8eb5a510ba16d390dd5174eb9
sanskar-successive <sanskar.mishra@successive.tech>
2023-11-07 10:03:56 +0530

    modified withDataFetch HOC

--- src/Assignments/Assignment-5/Ques16/withDataFetching.jsx
+++ src/Assignments/Assignment-5/Ques16/withDataFetching.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import axios from 'axios'
 
-const withDataFetching = WrappedComponent => {
-    const WithDataFecthing = ()=>{
+const withDataFetching = (OriginalComponent) => {
+    const EnhancedComponent = ()=>{
         const [data, setData] = useState([]);
         const apiURL = "https://jsonplaceholder.typicode.com/posts";
 
         const handleDataFetch = ()=>{
             const response = axios.get(apiURL);
             setData(response.data);
-
         }
+        
         return (
-            <WrappedComponent data={data} handleDataFetch = {handleDataFetch}/>
+            <OriginalComponent data={data} handleDataFetch = {handleDataFetch}/>
         )
     }
-    return WithDataFecthing;
+    return EnhancedComponent;
 }
 export default withDataFetching;
